d78ed37cc1c6bd3245fd9ac2864fad59082007e3
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-07-31 21:29:33 -0600

    Scrapped project (It got corrupted)

--- A01_BlockoutShooter/Source/A01_BlockoutShooter/A01_BlockoutShooter.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "A01_BlockoutShooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, A01_BlockoutShooter, "A01_BlockoutShooter" );
