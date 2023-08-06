7fcf980ec9f5e2ae56fad95cff81fa43b341c474
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-08-06 05:54:51 -0600

    Changed project to c++ instead of blueprints

--- A01_BlockoutShooter/Source/A01_BlockoutShooter/A01_BlockoutShooter.cpp
+++ A01_BlockoutShooter/Source/A01_BlockoutShooter/A01_BlockoutShooter.cpp
@@ -1,6 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "A01_BlockoutShooter.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, A01_BlockoutShooter, "A01_BlockoutShooter" );
+ 
--- A01_BlockoutShooter/Source/A01_BlockoutShooter/A01_BlockoutShooterCharacter.h
+++ A01_BlockoutShooter/Source/A01_BlockoutShooter/A01_BlockoutShooterCharacter.h
@@ -1,29 +1,66 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputActionValue.h"
 #include "A01_BlockoutShooterCharacter.generated.h"
 
-UCLASS()
-class A01_BLOCKOUTSHOOTER_API AA01_BlockoutShooterCharacter : public ACharacter
+
+UCLASS(config=Game)
+class AA01_BlockoutShooterCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+	
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+
 public:
-	// Sets default values for this character's properties
 	AA01_BlockoutShooterCharacter();
+	
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
 
-	// Called to bind functionality to input
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+			
+
+protected:
+	// APawn interface
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// To add mapping context
+	virtual void BeginPlay();
 
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
+
